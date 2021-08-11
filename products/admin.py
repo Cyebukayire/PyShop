7801382d09d9d59a88f100ec71ea48a9de2168b8
Cyebukayire <peacecyebukayire@gmail.com>
2021-08-11 23:14:45 +0200

    Add products and offers to admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product, Offer
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Offer, OfferAdmin)
+
--- products/apps.py
+++ products/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ProductsConfig(AppConfig):
     name = 'products'
+# user: I-am-a-guest pass: the guest is here
