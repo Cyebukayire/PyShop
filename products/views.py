2fd0cfd6f279d19acabba3e857371928fa70cfe1
Cyebukayire <peacecyebukayire@gmail.com>
2021-08-12 10:12:10 +0200

    Add migrations

--- products/views.py
+++ products/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 
 def index(request):
-    return HttpResponse('Products...')
+    products = Product.objects.all()
+    return render(request, 'index.html',
+                  {'products': products})
 
 
 def new(request):
