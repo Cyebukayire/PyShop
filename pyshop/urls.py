11b6bc560a1d6eb405ec6434ab94f4eee32e015a
Cyebukayire <peacecyebukayire@gmail.com>
2021-08-17 22:52:08 +0200

    Add Images

--- pyshop/urls.py
+++ pyshop/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +23,5 @@ urlpatterns = [
     path('products', include('products.urls')),
     path('products/new/', include('products.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
